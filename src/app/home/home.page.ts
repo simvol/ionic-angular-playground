ee5a6c08619f138fcdd1801c441a14cf537c2cf2
Evgeny <isimvol@gmail.com>
2019-08-29 22:11:44 -0700

    Pizza order

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  pizzaName: string;
+  pizzaSize: number;
+  orders = [];
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+  submitOrder() {
+    if (!this.pizzaName || !this.pizzaSize) {
+      this.presentAlert();
+      return;
+    }
+    this.orders.push({ name: this.pizzaName, size: this.pizzaSize });
+    this.clearInptus();
+  }
+
+  clearInptus() {
+    this.pizzaName = '';
+    this.pizzaSize = null;
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Almost there!',
+      message: 'Please pick your pizza and size.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
